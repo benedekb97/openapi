2ab327408c9d356aaa9e58d63adb52dc00a312ca
Benedek Burgess <benedekb97@sch.bme.hu>
2024-10-03 19:01:42 +0100

    Hook up generator to command

--- src/Command/GenerateOpenAPICommand.php
+++ src/Command/GenerateOpenAPICommand.php
@@ -4,23 +4,25 @@ declare(strict_types=1);
 
 namespace Benedekb\OpenAPI\Command;
 
+use Benedekb\OpenAPI\Component\Generator\OpenApiGenerator;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
 #[AsCommand(name: 'openapi:generate')]
-class GenerateOpenAPICommand extends Command
+final class GenerateOpenAPICommand extends Command
 {
     public function __construct(
-        private string $filePath
+        private readonly string $filePath,
+        private readonly OpenApiGenerator $openApiGenerator
     ) {
         parent::__construct();
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-
+        $this->openApiGenerator->generate();
 
         return Command::SUCCESS;
     }
