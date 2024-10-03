0ef990b00b6b1d1c25b603674c800e4c3bc8296a
Benedek Burgess <benedekb97@sch.bme.hu>
2024-10-03 19:19:11 +0100

    Remove arrayNode and exchange with scalar

--- config/openapi.php
+++ config/openapi.php
@@ -6,7 +6,7 @@ return static function (DefinitionConfigurator $definition): void {
     $definition->rootNode()
         ->children()
             ->scalarNode('file_path')->defaultValue('contracts/openapi.json')->end()
-            ->arrayNode('skipped_routes')->scalarPrototype()->defaultValue(['/_error/'])->end()
+            ->scalarNode('skipped_routes')->defaultValue(['/_error/'])->end()
         ->end()
     ;
 };
